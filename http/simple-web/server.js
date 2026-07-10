eecdff82bc85151cb242191e490aef5ec15175d6
Daniel Sakyi <dannysakyi@gmail.com>
2026-07-09 21:21:11 -0400

    serving css and JS

--- http/simple-web/server.js
+++ http/simple-web/server.js
@@ -3,13 +3,31 @@ const fs = require("node:fs/promises");
 const server = http.createServer();
 
 server.on("request", async(req, res) => {
-    console.log(req.url);
-    console.log(req.method);
+    // console.log(req.url);
+    // console.log(req.method);
 
     if(req.url === "/" && req.method === "GET"){
         res.setHeader("Content-type", "text/html");
         
         const fileHandle = await fs.open("./public/index.html", "r");
+        const fileStream = fileHandle.createReadStream();
+        
+        fileStream.pipe(res);
+    }
+
+    if(req.url === "/styles.css" && req.method === "GET"){
+        res.setHeader("Content-type", "text/css");
+        
+        const fileHandle = await fs.open("./public/styles.css", "r");
+        const fileStream = fileHandle.createReadStream();
+        
+        fileStream.pipe(res);
+    }
+
+    if(req.url === "/script.js" && req.method === "GET"){
+        res.setHeader("Content-type", "text/javascript");
+        
+        const fileHandle = await fs.open("./public/script.js", "r");
         const fileStream = fileHandle.createReadStream();
         
         fileStream.pipe(res);
